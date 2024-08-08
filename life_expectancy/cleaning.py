82f5138eabbd30eb001eb82cc8ded4f09e16030e
Pedro Fonseca <pedro.goncalo.fonseca@parceiros.nos.pt>
2024-08-08 15:36:32 +0100

    update cleaning

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -13,31 +13,38 @@ def list_tsv_files():
         raise FileNotFoundError("No TSV files found in the data directory.")
     return tsv_files[0]
 
+
+def clean_values(value):
+    """Removes all non-numeric and non-decimal characters from a string."""
+    return ''.join(c for c in value if c.isdigit() or c == '.') if isinstance(value, str) else value
+
+
 def clean_data(country = "PT"):
     """
     This function read input file and applies data transformations to output csv file
     """
 
-    # Read data from file
+    # i) Read data from file
     file = list_tsv_files()
     df = pd.read_csv(file, sep="\t")
 
-    # Correct df and unpivot data
+    # ii) Correct df and unpivot data
     df[['unit', 'sex', 'age', 'region']] = df['unit,sex,age,geo\\time'].str.split(',', expand=True)
     df = df.drop(columns=['unit,sex,age,geo\\time'])
     df = pd.melt(df, id_vars=['unit', 'sex', 'age', 'region'], var_name='year', value_name='value')
 
-    # Cast year to int and value to float
+    # iii) Cast year to int and value to float
     df['year'] = df['year'].astype(int)
+    df['value'] = df['value'].apply(clean_values)
     df['value'] = pd.to_numeric(df['value'].str.replace(':', ''), errors='coerce')
 
-    # Remove nan
+    # iv) Remove nan
     df = df.dropna(subset=['value'])
 
-    # Filter data for country
+    # v) Filter data for country
     df = df[df['region'] == country]
 
-    # Save the df into CSV file 
+    # vi) Save the df into CSV file 
     current_folder = os.getcwd()
     output_file = f'{current_folder}/life_expectancy/data/{country.lower()}_life_expectancy.csv'
     df.to_csv(output_file, index=False)
