b2243953ed964798923b94671af27e3143d3b706
Pedro Fonseca <pedro.goncalo.fonseca@parceiros.nos.pt>
2024-09-22 20:40:35 +0100

    fix linter

--- life_expectancy/tests/test_regions.py
+++ life_expectancy/tests/test_regions.py
@@ -5,11 +5,15 @@ from life_expectancy.regions import Region, NON_COUNTRIES
 
 
 class TestRegion(unittest.TestCase):
+    """Test cases for the Region enum"""
+
     def test_region_enum(self):
+        """Test that the Region enum members are correctly defined"""
         self.assertTrue(hasattr(Region, "PT"))
         self.assertEqual(Region.PT.value, "PT")
 
     def test_countries_method(self):
+        """Test that the countries method returns the correct list of countries"""
         countries = Region.countries()
         country_values = [country.value for country in countries]
         for non_country in NON_COUNTRIES:
